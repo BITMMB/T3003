1d44b4bb74b3ba5a574361de9ee2e4de47bf26fb
BITMMB <antonrusakov@mail.ru>
2022-11-04 10:11:14 +0300

    1.3.9

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import NewTaskForm from "../NewTaskForm";
 import TaskList from "../TaskList";
 
-export const App = () => {
-  return (
-    <div className="todoapp">
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
-};
+export default class App extends Component {
+  state = {
+    data: [
+      { label: "label1", completed: false, editing: false, id: 1 },
+      { label: "label2", completed: true, editing: false, id: 2 },
+      { label: "label3", completed: false, editing: true, id: 3 },
+    ],
+  };
 
-export default App;
+  onDelited = (id) => {
+    this.setState(({ data }) => {
+      const newState = data.filter((element) => {
+        return element.id !== id;
+      });
+      return { data: newState };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todoapp">
+        <NewTaskForm />
+        <TaskList data={this.state.data} onDelited={this.onDelited} />
+      </div>
+    );
+  }
+}
